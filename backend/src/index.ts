f98edec06e3d25ac7257ab19edfb318508ddefe9
giorgio <giorgio.programmer@gmail.com>
2024-01-09 12:10:41 +0100

    starting to set up the database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,3 +18,18 @@ app.listen(5000, () =>
 console.log(
   "do not give up. With 💓 By Gio"
 );
+
+/*
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+
+//connections and listeneres
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log("Server Open & Connected To Database 🤟")
+    );
+  })
+  .catch((err) => console.log(err));
+*/
