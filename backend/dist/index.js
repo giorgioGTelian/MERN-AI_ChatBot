bf114f28752a2d6772e0d058839fab04199b413f
giorgio <giorgio.programmer@gmail.com>
2024-01-09 15:50:23 +0100

    COMMIT

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,38 +1,19 @@
-import express from "express";
-import { config } from "dotenv";
-config();
-const app = express();
-/* routes */
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-// GET
-// PUT
-// POST
-// DELETE
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+//connections and listeneres
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => console.log("Server Open & Connected To Database 🤟"));
+})
+    .catch((err) => console.log(err));
+//config
+/*todo  */
 // GET /api/products
 // GET /api/products/:id
 // POST /api/products
 // PUT /api/products/:id
 // DELETE /api/products/:id
-/* middlewares */
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-/* debugging */
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+// GET /api/products/:id/reviews
 console.log("do not give up. With 💓 By Gio");
-/*
-import app from "./app.js";
-import { connectToDatabase } from "./db/connection.js";
-
-//connections and listeneres
-const PORT = process.env.PORT || 5000;
-connectToDatabase()
-  .then(() => {
-    app.listen(PORT, () =>
-      console.log("Server Open & Connected To Database 🤟")
-    );
-  })
-  .catch((err) => console.log(err));
-*/ 
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,43 +1,5 @@
 import express from "express";
 import { config } from "dotenv";
-config();
-const app = express();
-
-/* routes */
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-
-// GET
-// PUT
-// POST
-// DELETE
-
-// GET /api/products
-// GET /api/products/:id
-// POST /api/products
-// PUT /api/products/:id
-// DELETE /api/products/:id
-
-/* middlewares */
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
-
-
-/* debugging */
-app.listen(5000, () =>
-  console.log("Server running on http://localhost:5000")
-);
-
-
-console.log(
-  "do not give up. With 💓 By Gio"
-);
-
-/*
 import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
 
@@ -50,4 +12,15 @@ connectToDatabase()
     );
   })
   .catch((err) => console.log(err));
-*/
+
+//config
+/*todo  */
+// GET /api/products
+// GET /api/products/:id
+// POST /api/products
+// PUT /api/products/:id
+// DELETE /api/products/:id
+// GET /api/products/:id/reviews
+console.log(
+  "do not give up. With 💓 By Gio"
+);
