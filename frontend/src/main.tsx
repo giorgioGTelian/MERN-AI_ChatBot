7062114ac26d6afe1db4f56f0ace64ef3da86f65
giorgio <giorgio.programmer@gmail.com>
2024-01-13 16:59:41 +0100

    setting up the theme with mui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+//import { createTheme } from '@mui/material'; 
+
+/* const theme = createTheme({
+  typography: {
+    fontFamily: 'Roboto',
+    allVariants: { color: "white" },
+  },
+}); */
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
