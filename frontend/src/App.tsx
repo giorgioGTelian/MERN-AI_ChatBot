6e16849b0632ae9c6e940e293bc580998f3f69eb
giorgio <giorgio.programmer@gmail.com>
2024-01-13 17:39:35 +0100

    started to fix the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,30 @@
-
-import './App.css'
+import Header from "./components/header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+/* import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound"; */
+//import { useAuth } from "./context/AuthContext";
+//import Footer from "./components/footer/footer";
 
 function App() {
-
+  //const auth = useAuth(); 
 
   return (
-    <>
-      <h1>App</h1>
-      <p>App</p>
-      
-    </>
-  )
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+{/*         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {auth?.isLoggedIn && auth.user && (
+          <Route path="/chat" element={<Chat />} />
+        )}
+        <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </main>
+  );
 }
 
-export default App
+export default App;
